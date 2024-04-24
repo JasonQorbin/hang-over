bd5d1fb300de58dddfd6b8338c9d8ddfe38706a2
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-04-24 20:51:05 +0200

    Bug: The last letter of each word was not showing.

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -4,7 +4,7 @@ function WordDisplay(props) {
     //Note for reviewer: The task wanted me to use Array.map, but I chose to use a simple for-loop to
     //easily generate the key that React needs. There is no other unique value I can use to I need to
     //create one.
-    for (let i = 0; i < outputLetters.length - 1; i++) {
+    for (let i = 0; i < outputLetters.length; i++) {
         if (props.revealedLetters.indexOf(i) == -1) {
             outputElements.push(<span className="word-display-letter" key={i}>_</span>);
         } else {
