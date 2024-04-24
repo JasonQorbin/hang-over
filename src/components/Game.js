6e78d25fad610b5f2b2f4bf0c3f04ecddfbaf3ab
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-04-24 10:43:34 +0200

    Fixed the application initialization
    Now showing a loading screen while the application is loading (reading the dictionary.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React from 'react';
+import dictionaryFile from '../assets/dictionary.txt';
+import LoadingScreen from "./LoadingScreen";
 
 function HangingMan() {
     return (
@@ -11,6 +13,9 @@ function HangingMan() {
 function WordDisplay(props) {
     let outputLetters = Array.from(props.currentWord);
     let outputElements = [];
+    //Note for reviewer: The task wanted me to use Array.map, but I chose to use a simple for-loop to
+    //easily generate the key that React needs. There is no other unique value I can use to I need to
+    //create one.
     for (let i = 0; i < outputLetters.length - 1; i++) {
         if (props.revealedLetters.indexOf(i) == -1) {
             outputElements.push(<span className="word-display-letter" key={i}>_</span>);
@@ -27,7 +32,7 @@ function WordDisplay(props) {
     )
 }
 
-function LetterTray() {
+function LetterTray(props) {
     return (
         <div id="letter-tray">
             Letter Tray
@@ -51,36 +56,101 @@ class Game extends React.Component {
         this.setRevealedLetters = this.setRevealedLetters.bind(this);
         this.setUsedLetters = this.setUsedLetters.bind(this);
         this.state = {
-            currentWord: "example",
+            dictionary : [],
+            currentWord: "",
             revealedLetters: [2],
             usedLetters: [],
+            applicationReady: false,
+            applicationLoading:false
         };
     }
 
+    chooseRandomWord(dictionary) {
+        return dictionary[Math.floor(Math.random() * (dictionary.length - 1))];
+    }
+
+    readDictionary() {
+        return new Promise(function(resolve) {
+            fetch(dictionaryFile)
+                .then(result => {
+                    return result.text();
+                })
+                .then(contents => {
+                    const lines = contents.split('\n');
+                    let chosenWords = [];
+                    let notFoundTheStartYet = true;
+                    for (let i = 0; i < lines.length; i++) {
+
+                        if (notFoundTheStartYet) { // Skip the preamble
+                            if (lines[i] === "START") {
+                                notFoundTheStartYet = false;
+                            }
+                        } else {
+                            //Only take words longer than 5 characters
+                            if (lines[i].length > 5) {
+                                chosenWords.push(lines[i]);
+                            }
+                        }
+                    }
+                    resolve(chosenWords);
+                });
+
+        });
+    }
+
     setCurrentWord(newWord) {
-        this.setState({currentWord: newWord});
+        this.setState({ currentWord: newWord });
     }
 
     setRevealedLetters(newLetters) {
-        this.setState({revealedLetters: newLetters });
+        this.setState({ revealedLetters: newLetters });
     }
 
     setUsedLetters(newLetters) {
         this.setState({usedLetters: newLetters });
     }
 
+    componentDidMount() {
+        const {applicationReady, applicationLoading} = this.state;
+
+        if (!applicationReady && !applicationLoading) {
+            console.log("Getting the application ready...")
+            this.getReady().then(r => {console.log("Application should be ready now...")});
+            this.setState({applicationLoading: true});
+        }
+    }
 
     render() {
-        return (
-            <div id="game">
-                <HangingMan/>
-                <WordAndLetters
-                    currentWord={this.state.currentWord}
-                    revealedLetters={this.state.revealedLetters}
-                    usedLetters={this.state.usedLetters}
-                />
-            </div>
-        )
+        if (!this.state.applicationReady) {
+            return (
+                <div>
+                    <LoadingScreen />
+                </div>
+            )
+        } else {
+            return (
+                <div id="game">
+                    <HangingMan/>
+                    <WordAndLetters
+                        currentWord={this.state.currentWord}
+                        revealedLetters={this.state.revealedLetters}
+                        usedLetters={this.state.usedLetters}
+                    />
+                </div>
+            )
+        }
+    }
+
+    async getReady() {
+        this.readDictionary().then((dictionaryWords) => {
+            const newWord = this.chooseRandomWord(dictionaryWords);
+            this.setState({
+                dictionary: dictionaryWords,
+                currentWord: newWord,
+                applicationReady: true,
+                applicationLoading: false
+            });
+        });
     }
 }
 
